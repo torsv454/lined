c643b2b7313e11b4f13f5326d68e08bba2cb425c
Tord <tord.svensson@gmail.com>
2018-07-30 08:55:38 +0200

    Moved tokenizer implementation to methods.

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -22,60 +22,60 @@ pub struct TokenInfo {
     column: usize,
 }
 
-impl <'a> Tokenizer<'a> {
+impl<'a> Tokenizer<'a> {
     fn info(&self) -> TokenInfo {
         TokenInfo {
             line: self.line,
             column: self.col,
         }
     }
-}
 
-fn word_or_token(tokenizer: &mut Tokenizer, token: Token, c: char) -> Token {
-    if tokenizer.buf.is_empty() {
-        token
-    } else {
-        let result = word(tokenizer);
-        tokenizer.buf.push(c);
-        result
-    }
-}
-
-fn word(tokenizer: &mut Tokenizer) -> Token {
-    let result: String = tokenizer.buf.drain(..).collect();
-    if let Ok(num) = result.parse() {
-        Token::NUM(tokenizer.info(), num)
-    } else {
-        Token::WORD(tokenizer.info(), result)
-    }
-}
-
-fn quoted_string(tokenizer: &mut Tokenizer) -> Token {
-    let mut escape = false;
-    while let Some(c) = tokenizer.chars.next() {
-        tokenizer.col += 1;
-        // println!("c = {}, escape = {}", c, escape);
-        match c {
-            '"' if !escape => break,
-            '"' => {
-                escape = false;
-                tokenizer.buf.push(c)
-            }
-            '\\' if !escape => escape = true,
-            '\\' => {
-                escape = false;
-                tokenizer.buf.push(c)
-            }
-            '\n' => {
-                tokenizer.line += 1;
-                tokenizer.col = 0;
-                tokenizer.buf.push(c);
-            }
-            _ => tokenizer.buf.push(c),
+    fn word_or_token(&mut self, token: Token, c: char) -> Token {
+        if self.buf.is_empty() {
+            token
+        } else {
+            let result = self.word();
+            self.buf.push(c);
+            result
         }
     }
-    let result: String = tokenizer.buf.drain(..).collect();
-    Token::STRING(tokenizer.info(), result)
+
+    fn word(&mut self) -> Token {
+        let result: String = self.buf.drain(..).collect();
+        if let Ok(num) = result.parse() {
+            Token::NUM(self.info(), num)
+        } else {
+            Token::WORD(self.info(), result)
+        }
+    }
+
+    fn quoted_string(&mut self) -> Token {
+        let mut escape = false;
+        while let Some(c) = self.chars.next() {
+            self.col += 1;
+            // println!("c = {}, escape = {}", c, escape);
+            match c {
+                '"' if !escape => break,
+                '"' => {
+                    escape = false;
+                    self.buf.push(c)
+                }
+                '\\' if !escape => escape = true,
+                '\\' => {
+                    escape = false;
+                    self.buf.push(c)
+                }
+                '\n' => {
+                    self.line += 1;
+                    self.col = 0;
+                    self.buf.push(c);
+                }
+                _ => self.buf.push(c),
+            }
+        }
+        let result: String = self.buf.drain(..).collect();
+        Token::STRING(self.info(), result)
+    }
 }
 
 impl<'a> Iterator for Tokenizer<'a> {
@@ -86,20 +86,20 @@ impl<'a> Iterator for Tokenizer<'a> {
             self.col += 1;
 
             match c {
-                '(' => return { let info = self.info(); Some(word_or_token(self,Token::LPAREN(info), '('))},
-                ')' => return  { let info = self.info(); Some(word_or_token(self,Token::LPAREN(info), ')'))},
-                '{' => return  { let info = self.info(); Some(word_or_token(self,Token::LBRACE(info), '{'))},
-                '}' => return  { let info = self.info(); Some(word_or_token(self,Token::RBRACE(info), '}'))},
+                '(' => return { let info = self.info(); Some(self.word_or_token(Token::LPAREN(info), '('))},
+                ')' => return  { let info = self.info(); Some(self.word_or_token(Token::LPAREN(info), ')'))},
+                '{' => return  { let info = self.info(); Some(self.word_or_token(Token::LBRACE(info), '{'))},
+                '}' => return  { let info = self.info(); Some(self.word_or_token(Token::RBRACE(info), '}'))},
                 '"' => // assert empty buffer 
-                    return Some(quoted_string(self)),
-                ' ' | '\t' | '\n' | '\r' => {self.line += 1; self.col = 0; if !self.buf.is_empty() { return Some(word(self)); }},
+                    return Some(self.quoted_string()),
+                ' ' | '\t' | '\n' | '\r' => {self.line += 1; self.col = 0; if !self.buf.is_empty() { return Some(self.word()); }},
                 _ => self.buf.push(c),
             }
         }
         if self.buf.is_empty() {
             None
         } else {
-            Some(word(self))
+            Some(self.word())
         }
     }
 }
